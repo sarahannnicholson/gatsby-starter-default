d2d43e2e6543b8dcd2e9435b4620f67c791705e2
sarah <sarah.ann.nicholson@gmail.com>
2018-10-20 10:07:58 -0700

    IE fix for localhost and readme update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Img from "gatsby-image"
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Img fixed={data.file.childImageSharp.fixed} />
   </Layout>
 )
+
+export const query = graphql`
+query {
+  file(relativePath: { regex: "/Bulbasaur/" }) {
+    childImageSharp {
+      fixed {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
